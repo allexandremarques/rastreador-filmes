f4e962670f3fbe341ac51289dd3e7ba11edc4266
Alexandre <allexandredev@gmail.com>
2026-03-01 14:44:36 -0300

    adiciona evento no btn Salvar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 console.log("O motor do Rastreador está ligado!");
+
+const formulario = document.querySelector('#form-adicionar')
+
+formulario.addEventListener('submit', function(evento) {
+    evento.preventDefault();
+    console.log("Interceptado! A página não recarregou.");
+});
+
